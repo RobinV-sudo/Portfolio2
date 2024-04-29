e175431613a44af0a25dcc9d4fbfb5d8aedbb73b
robin vorstedt robinvorstedt@outlook.com <robinvorstedt@outlook.com>
2024-04-29 17:29:50 +0900

    try new slideshow

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,7 @@ class PagesController < ApplicationController
   end
 
   def project
+    @photos = ["Spacetrip_-_1_ert0oz", "Spacetrip_-_2_xklcit", "Spacetrip_-_3_ls118i", "Spacetrip_-_4_xoyob0", "Spacetrip_-_5_ft0cqo"]
   end
 
 end
