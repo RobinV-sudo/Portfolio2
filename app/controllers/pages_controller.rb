68e7895d95b20ec98883b00168206a556b73d858
robin vorstedt robinvorstedt@outlook.com <robinvorstedt@outlook.com>
2024-07-08 19:24:51 +0900

    update on slideshow project

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,6 @@ class PagesController < ApplicationController
   end
 
   def project
-    @slideshow_images_names = ["Spacetrip_-_1_ert0oz", "Spacetrip_-_2_xklcit", "Spacetrip_-_3_ls118i", "Spacetrip_-_4_xoyob0", "Spacetrip_-_5_ft0cqo"]
-    @slideshow_selected_image_index = 1
   end
 
 end
