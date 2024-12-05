2ea11d036c0e699ec201921dba3a8452c51164b6
robin vorstedt robinvorstedt@outlook.com <robinvorstedt@outlook.com>
2024-12-05 14:45:11 +0900

    test with dropdown with javascript

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "about", to: 'pages#about'
   get "project", to: 'pages#project'
+  get "test", to: 'pages#test'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
