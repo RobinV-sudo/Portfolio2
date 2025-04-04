2b0f59341818e5e2d7e39cba32dc61c301ee023b
robin vorstedt robinvorstedt@outlook.com <robinvorstedt@outlook.com>
2025-04-04 15:05:19 +0900

    src-bundle.js update

--- config/dist/bundle.js
+++ config/dist/bundle.js
@@ -1,9 +1,2 @@
-/******/ (function() { // webpackBootstrap
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-// Entry point for the application
-console.log('Hello, Webpack!');
-/******/ })()
-;
+console.log("Hello, Webpack!");
 //# sourceMappingURL=bundle.js.map
