a2b082fe5647cf8c03f27def7a89308f33ceded6
robin vorstedt robinvorstedt@outlook.com <robinvorstedt@outlook.com>
2025-04-17 16:42:43 +0900

    router changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
-  get "/about", to: 'pages#about'
-  get "/project", to: 'pages#project'
-  get "/test", to: 'pages#test'
+  root to: 'pages#home'
+  get '/about", to: 'pages#about'
+  get '/project", to: 'pages#project'
+  get '/test", to: 'pages#test'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
