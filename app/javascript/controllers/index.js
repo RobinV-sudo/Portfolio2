dea937e5060351a36a1e491f8a9ad17d1ec7eba4
robin vorstedt robinvorstedt@outlook.com <robinvorstedt@outlook.com>
2025-04-17 16:45:26 +0900

    changes in javascript index.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,14 +1,1 @@
 // Entry point for the application
-const http = require('http');
-
-const hostname = '0.0.0.0'; // Use 0.0.0.0 to allow Heroku to bind to the correct IP
-const port = process.env.PORT || 8080; // Use the port provided by Heroku
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Webpack!\n');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
